7f0ad1af9dff5f9d7b465516f94d49683fab6433
umar azeem <125313441+Umar-azeem@users.noreply.github.com>
2024-12-03 20:25:14 +0500

    first commit

---
+++ src/component/Main.jsx
@@ -0,0 +1,172 @@
+import { useState } from "react";
+import TodoColSec from "./TodoColSec";
+function Main(){
+   const [drop,setdrop]=useState(false);
+ const handledrop = () =>{
+setdrop(!drop);
+ }
+    return(
+
+<>
+<div className="grid grid-cols-4 z-0 bg-[#2B2C37] w-screen h-screen text-white">
+<div className=" col-start-1 col-span-1 bg-[#2B2C37] md:border-r border-gray-400 grid-rows-4">
+<div className="lg:grid hidden py-6">
+<div className="px-6  "> 
+              <button
+                type="button"
+                className="relative my-1 inline-flex items-center justify-center rounded-md p-2 text-indigo-800"
+              >
+                <div className="flex  justify-center gap-3 items-center">
+                  <div className="flex flex-row justify-center items-center gap-[1px]">
+                    <hr className="w-2 h-6 rounded-sm bg-gradient-to-b from-violet-400 to-indigo-600 transform " />
+                    <hr className="w-2 h-6 rounded-sm bg-gradient-to-b from-violet-500 to-indigo-700 transform " />
+                    <hr className="w-2 h-6 rounded-sm bg-gradient-to-b from-violet-600 to-indigo-800 transform " />
+                  </div>
+                  <p className="text-3xl rounded-lg font-bold  text-white">
+                    Kanban
+                  </p>
+                </div>
+              </button>
+</div>
+<div className="space-y-5  border-gray-200 px-4 py-6">
+              <p className="text-lg mx-auto font-semibold text-gray-400">All boards (1)</p>
+</div> 
+<div className="grid  grid-rows-8 lg:grid-rows-6 ">
+            <div className="grid grid-cols-5 justify-center items-center  bg-violet-800  text-white hover:text-violet-800 hover:bg-white rounded-r-full border-gray-200 gap-5 px-2  mr-6">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-6 "
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 0 1-1.125-1.125M3.375 19.5h7.5c.621 0 1.125-.504 1.125-1.125m-9.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-7.5A1.125 1.125 0 0 1 12 18.375m9.75-12.75c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125m19.5 0v1.5c0 .621-.504 1.125-1.125 1.125M2.25 5.625v1.5c0 .621.504 1.125 1.125 1.125m0 0h17.25m-17.25 0h7.5c.621 0 1.125.504 1.125 1.125M3.375 8.25c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m17.25-3.75h-7.5c-.621 0-1.125.504-1.125 1.125m8.625-1.125c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M12 10.875v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125M13.125 12h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125M20.625 12c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5M12 14.625v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 14.625c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m0 1.5v-1.5m0 0c0-.621.504-1.125 1.125-1.125m0 0h7.5"
+                />
+              </svg>
+
+              <p className="text-lg font-semibold grid grid-cols-4">boards</p>
+            </div>
+            <div className="grid grid-cols-7 lg:grid-cols-3  rounded-r-full    hover:bg-white hover:text-violet-700 text-violet-800  border-gray-200 gap-5 px-4 py-2 mt-2 mr-8">
+              <svg 
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-6 "
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 0 1-1.125-1.125M3.375 19.5h7.5c.621 0 1.125-.504 1.125-1.125m-9.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-7.5A1.125 1.125 0 0 1 12 18.375m9.75-12.75c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125m19.5 0v1.5c0 .621-.504 1.125-1.125 1.125M2.25 5.625v1.5c0 .621.504 1.125 1.125 1.125m0 0h17.25m-17.25 0h7.5c.621 0 1.125.504 1.125 1.125M3.375 8.25c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m17.25-3.75h-7.5c-.621 0-1.125.504-1.125 1.125m8.625-1.125c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M12 10.875v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125M13.125 12h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125M20.625 12c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5M12 14.625v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 14.625c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m0 1.5v-1.5m0 0c0-.621.504-1.125 1.125-1.125m0 0h7.5"
+                />
+              </svg>
+
+              <p className="text-xs lg:text-lg font-semibold  col-span-4 lg:col-span-2 ">+ Create New Board</p>
+            </div>
+</div>
+<div className="lg:grid hidden  items-center grid-cols-3 justify-center  bg-slate-800  rounded-md hover:text-gray-500 text-violet-800  border-gray-200 gap-6 px-4 py-4 mx-5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-3 md:size-6 "
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+                />
+              </svg>
+
+              <div>
+                {" "}
+                <input
+             className="lg:me-2 mt-[4px] lg:mt-[0.3rem] h-3 w-6 lg:h-3.5 lg:w-8 appearance-none rounded-[0.4375rem] bg-black/25 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-white after:shadow-switch-2 after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ms-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-switch-1 checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-switch-3 focus:before:shadow-black/60 focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ms-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-switch-3 checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-white/25 dark:after:bg-surface-dark dark:checked:bg-primary dark:checked:after:bg-primary"
+             type="checkbox"
+             role="switch"
+             id="flexSwitchCheckDefault" />
+            <label className="inline-block ps-[0.15rem] hover:cursor-pointer"/>
+              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="size-3 lg:size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+                />
+              </svg>
+ </div>
+ <div className="relative">
+ <div className=" mr-5">
+            <div className="flex justify-center items-start  bg-violet-800  text-white hover:text-violet-800 hover:bg-white rounded-r-full border-gray-200 py-3  ">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+</svg>   
+<p className="text-lg font-semibold grid ">Hide Sidebar</p> 
+</div>          
+</div>
+</div>
+
+</div>
+</div>  
+<div className="grid col-span-4 lg:col-span-3 grid-rows-6 ">
+<header className="border-b border-gray-400">
+<div className="flex text-white justify-between items-center p-3 lg:p-6 ">
+            <p className="text-md  md:text-lg  font-bold">Weekly</p>
+            <div className="flex flex-row justify-center items-center">
+          <button className="flex justify-center items-center flex-row md:font-bold text-xs md:text-lg bg-violet-800  text-white hover:text-violet-800 hover:bg-white rounded-full border-gray-200 px-4 py-2  lg:mr-8"> 
+          <p className="text-2xl md:font-bold text-center"><svg class="h-6 w-6 "  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <line x1="12" y1="5" x2="12" y2="19" />  <line x1="5" y1="12" x2="19" y2="12" /></svg></p><p className=" flex-col hidden lg:flex">Add new task</p>  
+            </button>
+           <button onClick={handledrop}> 
+           <div>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-8 text-gray-400">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
+</svg>
+{true && drop && (
+  <div id="dropdown" className="z-20 absolute right-8 top-24 md:top-16 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+    <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+      <li>
+        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit Board</a>
+      </li>
+      <li>
+        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Clear Board</a>
+      </li>
+      <li>
+        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Delete Board</a>
+      </li>
+      <li>
+        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Reset Boards</a>
+      </li>
+    </ul>
+  </div>
+)}
+</div>
+</button>
+</div>
+ </div>
+</header>
+
+ <div className="relative bg-[#1a1b27] flex  row-span-5"><TodoColSec/></div>
+</div>
+</div>
+
+
+
+</>
+
+    )
+}
+export default Main; 
---
+++ src/component/TodoColSec.tsx
@@ -0,0 +1,43 @@
+function TodoColSec(){
+    return(
+     <>
+     <div className="absolute z-20 bottom-7  justify-end pr-7 bg-violet-800  text-white hover:text-violet-800 hover:bg-white rounded-r-full border-gray-200 py-3  ">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+</svg>   
+</div>
+
+     <section className="w-full grid grid-cols-4 z-10 gap-5 m-4  bg-[#1a1b27]  justify-center  ">
+    <div className="grid-rows-2 ">
+    <div className="row-span-1">
+        <p className="flex justify-center top-2 gap-1 items-center text-xl "><svg className="h-5 w-5 bg-red-900 text-red-900 rounded-full"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="12"  fill="currentColor" />  <circle cx="12" cy="12" r="9" /></svg>Todo (1)</p> 
+    </div>
+    <div className="row-span-1 text-sm font-mono grid grid-rows-5 gap-4  w-full h-auto text-gray-400 flex-col ">
+    <div className="w-auto  flex flex-col justify-center items-start p-2 g font-medium bg-gray-800 rounded-lg">
+        <p className=" ">S</p>
+        <p className="" >2 of 2 task</p>
+    </div>
+    <div className="w-auto text-md p-4 font-medium bg-gray-800 rounded-lg">
+        <p className="">S</p>
+        <p className="" >2 of 2 task</p>
+    </div>
+    <div className="w-auto text-md p-4 font-medium bg-gray-800 rounded-lg">
+        <p className="">S</p>
+        <p className="" >2 of 2 task</p>
+    </div>
+    <div className="w-auto text-md p-4 font-medium bg-gray-800 rounded-lg">
+        <p className="">S</p>
+        <p className="" >2 of 2 task</p>
+    </div>
+   </div>
+    </div>
+    <button className="grid col-span-end">
+    <div className="w-full h-full overflow-hidden  text-md  flex justify-center items-center font-medium  text-gray-400 bg-gray-800 rounded-lg">
+    <p className="text-2xl md:font-bold text-center"><svg className="h-6 w-6 "  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <line x1="12" y1="5" x2="12" y2="19" />  <line x1="5" y1="12" x2="19" y2="12" /></svg></p><p className=" flex-col hidden lg:flex"> New column</p>  
+    </div> 
+    </button>
+   </section>
+     </>
+    )
+}
+export default TodoColSec;
